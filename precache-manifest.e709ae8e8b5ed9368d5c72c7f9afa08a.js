5c86d40b6afd39ed8f811f99fca6c71b0c8c906e
Kiran Neupane <burgeon.kiran@gmail.com>
2020-08-28 11:43:24 +0545

    Updates

---
+++ precache-manifest.e709ae8e8b5ed9368d5c72c7f9afa08a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a87be6eab3c71e87e7b9e95278d8279",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a241fd6d7555370de8fc",
+    "url": "./static/css/main.92018274.chunk.css"
+  },
+  {
+    "revision": "ebca852b8af093b85c3e",
+    "url": "./static/js/2.a0424222.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a0424222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a241fd6d7555370de8fc",
+    "url": "./static/js/main.c8406c46.chunk.js"
+  },
+  {
+    "revision": "ba9f2cff3895a163e292",
+    "url": "./static/js/runtime-main.9620e84b.js"
+  }
+]);
